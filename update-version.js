fa5ba498e84be55677e587fed2915ac28c804cf0
raydner.silva <raydner.flexmobile@gmail.com>
2025-07-28 16:24:25 -0300

    ajuste

--- update-version.js
+++ update-version.js
@@ -1,22 +1,38 @@
-const fs = require('fs');
-const puppeteer = require('puppeteer');
+const fs = require("fs");
+const puppeteer = require("puppeteer");
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] });
+  const browser = await puppeteer.launch({
+    headless: "new",
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+    ],
+  });
   const page = await browser.newPage();
 
-  await page.goto('https://web.whatsapp.com', { waitUntil: 'networkidle2' });
-  await page.waitForSelector('#app', { timeout: 60000 });
+  await page.goto("https://web.whatsapp.com", { waitUntil: "networkidle2" });
+  await page.waitForSelector("#app", { timeout: 90000 });
 
-  await page.waitForFunction(() =>
-    typeof window.Debug !== 'undefined' && typeof window.Debug.VERSION !== 'undefined'
+  await page.waitForFunction(
+    () =>
+      typeof window.Debug !== "undefined" &&
+      typeof window.Debug.VERSION !== "undefined",
+    { timeout: 90000 } // 90 segundos
   );
 
-  const version = await page.evaluate(() => window.Debug.VERSION);
-  const versionArray = version.split('.').map(v => parseInt(v));
+  const content = await page.content();
+  console.log(content);
 
-  fs.writeFileSync('baileys-version.json', JSON.stringify({ version: versionArray }, null, 2));
-  console.log('✅ Versão atualizada:', versionArray);
+  const version = await page.evaluate(() => window.Debug.VERSION);
+  const versionArray = version.split(".").map((v) => parseInt(v));
+
+  fs.writeFileSync(
+    "baileys-version.json",
+    JSON.stringify({ version: versionArray }, null, 2)
+  );
+  console.log("✅ Versão atualizada:", versionArray);
 
   await browser.close();
 })();
