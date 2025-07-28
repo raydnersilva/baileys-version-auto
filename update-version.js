86c2c5523a2efbb4437a04b07bcc55c90301d67a
raydner.silva <raydner.flexmobile@gmail.com>
2025-07-28 16:33:21 -0300

    ajuste

--- update-version.js
+++ update-version.js
@@ -8,10 +8,20 @@ const puppeteer = require("puppeteer");
       "--no-sandbox",
       "--disable-setuid-sandbox",
       "--disable-dev-shm-usage",
+      "--disable-gpu",
+      "--window-size=1920,1080",
+      "--disable-background-timer-throttling",
+      "--disable-renderer-backgrounding",
+      "--disable-backgrounding-occluded-windows",
     ],
   });
   const page = await browser.newPage();
 
+  await page.setUserAgent(
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " +
+      "(KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+  );
+  
   await page.goto("https://web.whatsapp.com", { waitUntil: "networkidle2" });
   await page.waitForSelector("#app", { timeout: 90000 });
 
